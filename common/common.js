48444d8844f47422dff1d8fe8654422ac76b779e
Ranjan095 <iranjan095@gmail.com>
2022-12-18 18:14:13 +0530

    button is updated

--- common/common.js
+++ common/common.js
@@ -1,4 +1,4 @@
-const rootFolder = "C:/Users/Rajesh/Desktop/tall-question-6900"
+const rootFolder = "C:/Users/RanjanKr/Desktop/123456/tall-question-6900"
 const navBarElement = document.querySelector("#nav-bar");
 const orderSettingBarElement = document.querySelector("#order-settings1");
 const orderSettingsModal =document.querySelector("#order-settings-modal");
